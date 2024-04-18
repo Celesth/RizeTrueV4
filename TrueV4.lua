34da3631da3efb43f124c7e4b40ebd1d91ca5614
NozeLanze <167052643+DerNoze@users.noreply.github.com>
2024-04-18 21:47:50 +0530

    Update TrueV4.lua

--- TrueV4.lua
+++ TrueV4.lua
@@ -3,6 +3,8 @@ local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mooner
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/Moonerri/Rizee/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/Moonerri/Rizee/master/Addons/InterfaceManager.lua"))()
 
+local currentDate = os.date("%B %d, %Y") -- Get current date
+local playerCount = game.Players:GetPlayers()
 
 ------------------------------------
 local Window = Fluent:CreateWindow({
@@ -11,7 +13,7 @@ local Window = Fluent:CreateWindow({
     TabWidth = 120,
     Size = UDim2.fromOffset(560, 430),
     Acrylic = false, -- The blur may be detectable, setting this to false disables blur entirely
-    Theme = "Dark",
+    Theme = "Darker",
     MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
 })
 
@@ -33,23 +35,30 @@ local Options = Fluent.Options
 do
 Fluent:Notify({
     Title = "Rize [True V4]",
-    Content = "Sucessfully Injected",
+    Content = "Modules.Loading",
     SubContent = "V4", -- Optional
     Duration = 5 -- Set to nil to make the notification not disappear
 })
 
-
+task.wait(2)
 
 Tabs.Main:AddParagraph({
     Title = "True V4",
-    Content = "True V4 is in development\nCreated On 9:39PM, Sunday, March 24"
+    Content = "True V4 is in development Created On 9:39PM, Sunday, March 24"
 })
+Tabs.Dev:AddParagraph({
+    Title = ("Current Date:" .. currentDate .. "Player" ..playerCount)
+})
+
+
+print("Current date: " .. currentDate)
+print("Number of in-game players: " .. playerCount)
 
 
 
 Tabs.Dev:AddButton({
-    Title = "Iframe [Current]",
-    Description = "Gets You The Ifram Position.",
+    Title = "Iframe [Humanoid Position]",
+    Description = "Gets You The Iframes Of Your Currently Position.",
     Callback = function()
     Window:Dialog({
         Title = "Iframe [Current]",
@@ -64,6 +73,7 @@ Tabs.Dev:AddButton({
 
                 local position = humanoidRootPart.Position
                 print(position) -- This would output the position in the output window.
+                setclipboard(tostring(Game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position))
                 end
             },
             {
@@ -81,138 +91,33 @@ Tabs.Dev:AddToggle("Print", {
     Title = "Printing Button.",
     Description = "Print's On Or Off",
     Default = false,
-    Callback = function()
-                print("hel")
-            end
-})
-
-
-
-local Dropdown = Tabs.Main:AddDropdown("Dropdown", {
-    Title = "Dropdown",
-    Values = {
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"
-    },
-    Multi = false,
-    Default = 1,
-})
-
-Dropdown:SetValue("four")
-
-Dropdown:OnChanged(function(Value)
-    print("Dropdown changed:", Value)
-    end)
-
-
-
-local MultiDropdown = Tabs.Main:AddDropdown("MultiDropdown", {
-    Title = "Dropdown",
-    Description = "You can select multiple values.",
-    Values = {
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"
-    },
-    Multi = true,
-    Default = {
-        "seven", "twelve"
-    },
-})
-
-MultiDropdown:SetValue({
-    three = true,
-    five = true,
-    seven = false
-})
-
-MultiDropdown:OnChanged(function(Value)
-    local Values = {}
-    for Value, State in next, Value do
-    table.insert(Values, Value)
-    end
-    print("Mutlidropdown changed:", table.concat(Values, ", "))
-    end)
-
-
-
-local Colorpicker = Tabs.Main:AddColorpicker("Colorpicker", {
-    Title = "Colorpicker",
-    Default = Color3.fromRGB(96, 205, 255)
-})
-
-Colorpicker:OnChanged(function()
-    print("Colorpicker changed:", Colorpicker.Value)
-    end)
-
-Colorpicker:SetValueRGB(Color3.fromRGB(0, 255, 140))
-
-
-
-local TColorpicker = Tabs.Main:AddColorpicker("TransparencyColorpicker", {
-    Title = "Colorpicker",
-    Description = "but you can change the transparency.",
-    Transparency = 0,
-    Default = Color3.fromRGB(96, 205, 255)
-})
-
-TColorpicker:OnChanged(function()
-    print(
-        "TColorpicker changed:", TColorpicker.Value,
-        "Transparency:", TColorpicker.Transparency
-    )
-    end)
-
-
-
-local Keybind = Tabs.Main:AddKeybind("Keybind", {
-    Title = "KeyBind",
-    Mode = "Toggle", -- Always, Toggle, Hold
-    Default = "LeftControl", -- String as the name of the keybind (MB1, MB2 for mouse buttons)
-
--- Occurs when the keybind is clicked, Value is `true`/`false`
-    Callback = function(Value)
-    print("Keybind clicked!", Value)
-    end,
-
--- Occurs when the keybind itself is changed, `New` is a KeyCode Enum OR a UserInputType Enum
-    ChangedCallback = function(New)
-    print("Keybind changed!", New)
+    Callback = function(bool)
+            print("on")
     end
 })
 
--- OnClick is only fired when you press the keybind and the mode is Toggle
--- Otherwise, you will have to use Keybind:GetState()
-Keybind:OnClick(function()
-    print("Keybind clicked:", Keybind:GetState())
-    end)
 
-Keybind:OnChanged(function()
-    print("Keybind changed:", Keybind.Value)
-    end)
-
-task.spawn(function()
-    while true do
-    wait(1)
-
--- example for checking if a keybind is being pressed
-    local state = Keybind:GetState()
-    if state then
-    print("Keybind is being held down")
-    end
-
-    if Fluent.Unloaded then break end
-    end
-    end)
-
-Keybind:SetValue("MB2", "Toggle") -- Sets keybind to MB2, mode to Hold
-
-
-local Input = Tabs.Main:AddInput("Input", {
-    Title = "Input",
-    Default = "Default",
-    Placeholder = "Placeholder",
+local Input = Tabs.Dev:AddInput("Input", {
+    Title = "Teleport[Instant]",
+    Default = "",
+    Placeholder = "UserName",
     Numeric = false, -- Only allows numbers
     Finished = true, -- Only calls callback when you press enter
-    Callback = function(Value)
-    print("Input changed:", Value)
+    Callback = function(modules)
+        print(modules)
+    local player = game.Players.LocalPlayer
+    local usernameToTeleport = modules -- Replace with the desired username
+
+    local function teleportToPlayer()
+    local targetPlayer = game.Players:FindFirstChild(usernameToTeleport)
+    if targetPlayer then
+    player.Character:SetPrimaryPartCFrame(targetPlayer.Character.PrimaryPart.CFrame)
+    else
+        print("Player not found.")
+    end
+    end
+
+    teleportToPlayer()
     end
 })
 
@@ -248,10 +153,12 @@ SaveManager:BuildConfigSection(Tabs.Settings)
 
 Window:SelectTab(1)
 
+task.wait(3)
+
 Fluent:Notify({
-    Title = "RizeV4 [ Rize True V4]",
-    Content = "The script has been loaded.",
-    Duration = 8
+    Title = "Rize V4 [ Rize True V4]",
+    Content = "Injected: Ready To Go.",
+    Duration = 5
 })
 
 -- You can use the SaveManager:LoadAutoloadConfig() to load a config
